7eba9e93dc113068522549bf4a6badd9a9d68327
yanzhen74 <yanzhen@csu.ac.cn>
2017-02-05 23:14:49 +0800

    feat: add model Items
    Model for list Items and associated migration
    
    none

--- lists/tests.py
+++ lists/tests.py
@@ -4,11 +4,12 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
+from lists.models import Item
+
 import re
 
 
 class HomePageTest(TestCase):
-
     @staticmethod
     def remove_csrf(html_code):
         csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
@@ -46,3 +47,22 @@ class HomePageTest(TestCase):
             self.remove_csrf(response.content.decode()),
             self.remove_csrf(expected_html)
         )
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
