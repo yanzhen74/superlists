e53301778f0bc5e7347f83688f274690fab84494
yanzhen74 <yanzhen@csu.ac.cn>
2017-02-05 22:39:59 +0800

    Can process one and only one to-do item

--- lists/tests.py
+++ lists/tests.py
@@ -4,9 +4,15 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
+import re
 
 
 class HomePageTest(TestCase):
+
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -15,8 +21,28 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqual(
+            self.remove_csrf(response.content.decode()),
+            self.remove_csrf(expected_html)
+        )
         # self.assertTrue(response.content.startswith(b'<html>'))
         # self.assertIn(b'<title>To-Do lists</title>', response.content)
         # self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'},
+            request=request
+        )
+        self.assertEqual(
+            self.remove_csrf(response.content.decode()),
+            self.remove_csrf(expected_html)
+        )
