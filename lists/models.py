a7d51e110bccabf347bd5f27381d9ca3e77a7d72
yanzhen74 <yanzhen@csu.ac.cn>
2017-02-06 16:54:45 +0800

    feat: add model List
    1. List is the parent of Item
    2. change models and views
    
    none

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 
+class List(models.Model):
+    # list =
+    pass
+
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
+
+
