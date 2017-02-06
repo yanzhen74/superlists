7c776166cd2b53e49f363626b3f8ac9e8a39d960
yanzhen74 <yanzhen@csu.ac.cn>
2017-02-06 16:15:37 +0800

    feat: new URL, view and template to display lists
    1. add list.html / view_list and there url
    2. delete todo list from home.html and only show input view
    
    none

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -85,6 +85,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # 弗朗西斯获得了他的唯一URL
         francis_list_url = self.browser.current_url
--- lists/views.py
+++ lists/views.py
@@ -7,7 +7,10 @@ def home_page(request):
     # return HttpResponse('<html><title>To-Do lists</title></html>')
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
