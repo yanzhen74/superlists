c4e106f283f66f5abea539fc99396dd8eed9351d
yanzhen74 <yanzhen@csu.ac.cn>
2017-02-07 10:08:26 +0800

    fix: can not find static path when test
    1. replace LiveServerTestCase with StaticLiveServerTestCase
    2. todo: how to assign static address in base.html so that works both dev and production
    
    none

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(1)
@@ -111,7 +112,6 @@ class NewVisitorTest(LiveServerTestCase):
     def test_layout_and_styling(self):
         # 伊迪斯访问首页
         self.browser.get(self.live_server_url)
-        time.sleep(10)
         self.browser.set_window_size(1024, 512)
 
         # 她看到输入框完美地居中显示
